60d8c4e8ff3714d1441fc7699a9390529990da44
emilyreddy <emreddy@icloud.com>
2022-07-04 14:08:20 -0400

    updated player function

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -2,6 +2,7 @@ const Player = require('../lib/Player');
 const Potion = require('../lib/Potion');
 
 jest.mock('../lib/Potion');
+
 test('creates a player object', () => {
     const player = new Player('Dave');
   
@@ -13,4 +14,24 @@ test('creates a player object', () => {
         expect.arrayContaining([expect.any(Object)])
       );
   });
+
+  test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+  
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+  });
+
+  test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+  
+    expect(player.getInventory()).toEqual(expect.any(Array));
+  
+    player.inventory = [];
+  
+    expect(player.getInventory()).toEqual(false);
+  });
+  
   
